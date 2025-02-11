5d75cf74999dea35f2e23a7628215e43e41fdf41
Tatiana Retiukhina <taretiuk@student.42berlin.de>
2025-02-11 16:54:00 +0100

    fixup! new_feature: movement keys works but directions not correct

--- src/init_player/init_player.c
+++ src/init_player/init_player.c
@@ -6,7 +6,7 @@
 /*   By: taretiuk <taretiuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 15:12:06 by taretiuk          #+#    #+#             */
-/*   Updated: 2025/02/10 18:32:56 by taretiuk         ###   ########.fr       */
+/*   Updated: 2025/02/11 16:32:42 by taretiuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,10 @@ void	init_player(t_game *game)
 {
 	game->player.x = WIDTH / 2;
 	game->player.y = HEIGHT / 2;
-	game->player.dir_x = cos(0);
-	game->player.dir_y = sin(0);
+	game->player.dir_x = -1.0;
+	game->player.dir_y = 0.0;
+	game->player.plane_x = 0.0;
+	game->player.plane_y = 0.66;
 	game->player.move_speed = 3;
 	draw_player(game, (int)game->player.x, (int)game->player.y);
 }
